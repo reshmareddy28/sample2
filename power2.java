2f5e17838abc2798940ca173ebb6c3c9492a04bf
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-09 12:16:06 +0530

    Update and rename sumnum.java to power2.java

---
+++ power2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Ideone
+{
+	 static boolean isPowerOfTwo(int n)
+    {
+        if (n == 0)
+            return false;
+         
+        while (n != 1)
+        {
+            if (n % 2 != 0)
+                return false;
+            n = n / 2;
+        }
+        return true;
+    }
+ 
+    
+    public static void main(String args[])
+    {
+    	Scanner sc=new Scanner(System.in);
+    	int n=sc.nextInt();
+        if (isPowerOfTwo(n))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+ 
+       
+    }
+}
+ 
