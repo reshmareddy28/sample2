eb4bf689d07a7f3f91ca7432c77870913def0b6d
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-09 11:26:21 +0530

    Update and rename range.java to cnum.java

---
+++ cnum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Ideone 
+{
+
+    public static void main(String[] args) 
+    {
+    	Scanner sc=new Scanner(System.in);
+    	int num=sc.nextInt();
+     int count = 0;
+
+        while(num != 0)
+        {
+            // num = num/10
+            num /= 10;
+            ++count;
+        }
+
+        System.out.println(count);
+    }
+}
