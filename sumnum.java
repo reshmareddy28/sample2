57d757313d756862ad768bb5a0a25ff8ef4eed36
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-09 12:09:35 +0530

    Update and rename avgnum.java to sumnum.java

---
+++ sumnum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	int sum;
+       Scanner sc=new Scanner(System.in);
+ 
+       
+      int n=sc.nextInt();
+       int m=sc.nextInt();
+	sum=n+m;
+	System.out.println(sum);
+	}
+}
