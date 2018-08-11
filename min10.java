7152591d263a4c3afd3d17ec44c618a2e8f4de7e
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 11:04:23 +0530

    Update and rename div7.java to min10.java

---
+++ min10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int[] a=new int[10];
+		for(int i=0;i<10;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<10;i++)
+		{
+			for(int j=0;j<10-i-1;j++)
+			{
+				if(a[j]>a[j+1])
+				{
+					int temp=a[j];
+					a[j]=a[j+1];
+					a[j+1]=temp;
+				}
+			}
+		}
+		System.out.println(a[0]);
+	}
+}
