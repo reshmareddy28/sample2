f9ae47bec425708645cb1a22105e50a68a5c04a4
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 10:11:07 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		System.out.println(str);
+	}
+}
