348f3b3efeb487be0040df1e8b7a83bc2c964ff2
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 10:49:13 +0530

    Update and rename prime.java to pow2.java

---
+++ pow2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n *=2;
+		System.out.println(n);
+	}
+}
