9208224897f99f11bb90b4f431f8c166302b971e
reshmareddy28 <41780060+reshmareddy28@users.noreply.github.com>
2018-08-11 12:07:36 +0530

    Create celsi.java

---
+++ celsi.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int celsius=n+273;
+		System.out.println(celsius);
+	}
+}
